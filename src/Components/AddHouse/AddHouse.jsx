9592b7d5a35e14c50fb7c345f824cb476ed7f57a
SureshYarram <sureshchowdary540@gmail.com>
2022-03-14 11:16:57 +0530

    change

---
+++ src/Components/AddHouse/AddHouse.jsx
@@ -0,0 +1,93 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Rentals } from "../Rentals/Rentals";
+
+export const AddHouse = () => {
+
+
+   const [formData,setFormData] = useState({});
+   const [result,setResult] = useState([])
+   
+        
+
+      useEffect(()=>{
+        GetData();
+      },[])
+       const Handlechange = (e)=>{
+         const {className,value} = e.target;
+
+         if([className]=="married"&&[className].checked===true){
+          setFormData({...formData,married:"married"})
+         }
+         setFormData({...formData,[className]:value})
+       }
+      const Handlesubmit = (e)=>{
+           e.preventDefault();
+           axios.post("http://localhost:8080/houses",formData).then(()=>GetData());
+      }
+
+      const GetData = ()=>{
+        axios.get("http://localhost:8080/houses").then((res)=>{
+                  setResult([...res.data])
+        })
+      }
+
+      function sortbyid(){
+  let res = result.sort((a,b)=>{
+    if(a.id<b.id) return -1
+    return 0
+  })
+  setResult([...res])
+      }
+      function  rentsortinc(){
+        let res = result.sort((a,b)=>{
+          if(a.rent<b.rent) return -1
+          return 0
+        })
+        setResult([...res])
+      }
+      function  rentsortdec(){
+        let res = result.sort((a,b)=>{
+          if(b.rent<a.rent) return -1
+          return 0
+        })
+        setResult([...res])
+      }
+  return (
+    <div className="addHouseContainer">
+      <form onSubmit={Handlesubmit}>
+        <label>name</label>
+        <input onChange={Handlechange} type="text" className="name" value={formData.name} />
+        <br />
+        <label>ownerName</label>
+        <input onChange={Handlechange} value={formData.ownerName} type="text" className="ownerName" required="true" />
+        <br />
+        <label>address</label>
+        <input onChange={Handlechange} value={formData.address} type="text" className="address" required />
+        <br />
+        <label>areaCode</label>
+        <input onChange={Handlechange} value={formData.areaCode} type="text" className="areaCode" required />
+        <br />
+        <label>rent</label>
+        <input onChange={Handlechange} value={formData.rent} type="text" className="rent" required />
+        <br />
+        <label>preferredTenant</label>
+        <br />
+        <label>bachelor</label>
+        <input onChange={Handlechange} checked={formData.bachelor} type="checkbox" className="bachelor" />
+        <br />
+        <label>married</label>
+        <input onChange={Handlechange} checked={formData.married} type="checkbox" className="married" />
+        <br />
+        <label>image</label>
+        <input onChange={Handlechange} value={formData.image} type="text" className="image" required="false"/>
+        <br />
+        <input className="submitBtn" type="submit" />
+      </form>
+
+      <div>
+        <Rentals result = {result } sortbyid={sortbyid} rentsortinc={rentsortinc} rentsordec={rentsortdec} />
+      </div>
+    </div>
+  );
+};
---
+++ src/Components/Rentals/Rentals.jsx
@@ -0,0 +1,55 @@
+import "./Rentals.css";
+
+export const Rentals = ({result,sortbyid,rentsortinc,rentsortdec}) => {
+  return (
+    <div className="rentalContainer">
+      <div className="sortingButtons">
+        <button onClick={()=>sortbyid()} className="sortById">Sort by ID</button>
+        <button onClick={()=>rentsortinc()} className="sortByRentAsc">Rent Low to high</button>
+        <button onClick={()=>rentsortdec()}className="sortByRentDesc">Rent High to low</button>
+        <button className="sortByAreaAsc">Area Low to high</button>
+        <button className="sortByAreaDesc">Area High to Low</button>
+      </div>
+      <input
+        className="searchAddress"
+        type="text"
+        placeholder="Search Address"
+      />
+      <table className="table" border="1">
+        <thead>
+          <tr>
+            <th>Sl.no.</th>
+            <th>Name</th>
+            <th>Owner Name</th>
+            <th>Address</th>
+            <th>Area Code</th>
+            <th>Rent</th>
+            <th>Available For</th>
+            <th>Image</th>
+          </tr>
+        </thead>
+        <tbody>
+          {result.map((house, index) => {
+            return (
+              <tr key={house.id} className="houseDetails">
+                <td className="houseId">{house.id}</td>
+                <td className="houseName">{house.name} </td>
+                <td className="ownersName">{house.ownerName}</td>
+                <td className="address">{house.address}</td>
+                <td className="areaCode">{house.areaCode}</td>
+                <td className="rent">{house.rent}</td>
+                <td className="preferredTenants">
+                  {/* Show text Both or Bachelors or Married based on values */}
+                  {house.bachelor}
+                </td>
+                <td className="houseImage">
+                  <img src={house.image} alt="house" />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
