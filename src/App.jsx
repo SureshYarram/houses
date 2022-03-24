dd4b5ed982082a580b0a50a443171cab961adb2e
SureshYarram <sureshchowdary540@gmail.com>
2022-03-24 18:13:45 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
 import "./App.css";
 import { Rentals } from "./Components/Rentals/Rentals";
 import { AddHouse } from "./Components/AddHouse/AddHouse";
-import { useState } from "react";
-import axios from "axios";
-
+import {useState} from 'react'
 function App() {
-  
-  const [show ,setShow ] = useState(true);
-  const [data,setData] = useState([])
-
-  const getData = ()=>{
-    axios.get("http://localhost:8080/houses").then((res)=>{
-      setData(res.data)
-    })
-  }
+  const [change, setChange] = useState(false);
   return (
-    <div className="App" onClick={()=>{
-      setShow(!show)
-    }}>
-      {show ? "show rRentals" : "Add House"}
-      <button className="toggleForm">
+    <div className="App">
+      <button className="toggleForm" onClick={()=> setChange(!change)}>
+        {change ? "Show Rentals" : 'Add House'}
         {/* Show text Add House or Show Rentals based on state */}
-        {/* <h3>Add House</h3> */}
-        < AddHouse/>
       </button>
+        {change ?  <AddHouse /> : <Rentals />}
       {/* Show component based on state */}
-{show ? <AddHouse getData={getData}/> :<Rentals data={data} setData={setData}/>}
       <br />
     </div>
   );
