dce7755a637791f745b6840c0167dbe57f9cc44c
SureshYarram <sureshchowdary540@gmail.com>
2022-03-24 18:09:26 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import "./App.css";
 import { Rentals } from "./Components/Rentals/Rentals";
 import { AddHouse } from "./Components/AddHouse/AddHouse";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  
+  const [show ,setShow ] = useState(true);
+  const [data,setData] = useState([])
+
+  const getData = ()=>{
+    axios.get("http://localhost:8080/houses").then((res)=>{
+      setData(res.data)
+    })
+  }
   return (
-    <div className="App">
+    <div className="App" onClick={()=>{
+      setShow(!show)
+    }}>
+      {show ? "show rRentals" : "Add House"}
       <button className="toggleForm">
         {/* Show text Add House or Show Rentals based on state */}
-        <h3>Add House</h3>
+        {/* <h3>Add House</h3> */}
+        < AddHouse/>
       </button>
       {/* Show component based on state */}
-      < AddHouse/>
+{show ? <AddHouse getData={getData}/> :<Rentals data={data} setData={setData}/>}
       <br />
     </div>
   );
--- src/Components/Rentals/Rentals.jsx
+++ src/Components/Rentals/Rentals.jsx
@@ -1,19 +1,93 @@
-import "./Rentals.css";
 
-export const Rentals = ({result,sortbyid,rentsortinc,rentsortdec,sortareainc,sortareadec}) => {
+
+import {useEffect,useState} from 'react'
+import "./Rentals.css";
+import axios from 'axios'
+export const Rentals = () => {
+  const [data, setData] = useState([]);
+  const [change, setChange] = useState(false);
+  const getDataFromServer = async () => {
+    try {
+      const get = await axios.get('http://localhost:8080/houses');
+      setData(get.data);
+    }
+    catch(e){
+      console.log('error in getDataFromServer', e);
+    }
+  }
+  const handleRent = (value) =>{
+    if(value === 'low'){
+      let data2 = data.sort((a,b)=>{
+        return ((+a.rent) -  (+b.rent))
+      })
+      console.log(data2)
+      setData(data2)
+      setChange(!change)
+    
+    } else {
+      let data2 = data.sort((a,b)=>{
+        return ((+b.rent) -  (+a.rent))
+      })
+      setData(data2)
+      setChange(!change)
+    }
+  }
+  useEffect(()=>{
+
+  }, [change])
+  const handleId = () => { 
+      let data2 = data.sort((a,b)=>{
+        return ((+a.id) -  (+b.id))
+      })
+      setData(data2)
+      setChange(!change)
+    
+    
+  }
+  const handleArea = (value) => {
+    if(value === 'low'){
+      console.log('here')
+      let data2 = data.sort((a,b)=>{
+        return ((+a.areaCode) -  (+b.areaCode))
+      })
+      console.log(data2)
+      setData(data2)
+      setChange(!change)
+    
+    } else {
+      let data2 = data.sort((a,b)=>{
+        return ((+b.areaCode) -  (+a.areaCode))
+      })
+      console.log(data2)
+      setData(data2)
+      setChange(!change)
+    }
+  }
+  const handleAreaCode = (value) =>{
+    if(!value) getDataFromServer()
+    let k = data.filter(person => String(person.address).startsWith(value))
+    if(!k.length > 0) return
+    setData(k)
+    setChange(!change)
+    
+  }
+  useEffect(()=>{
+    getDataFromServer();
+  }, [])
   return (
     <div className="rentalContainer">
       <div className="sortingButtons">
-        <button onClick={()=>sortbyid()} className="sortById">Sort by ID</button>
-        <button onClick={()=>rentsortinc()} className="sortByRentAsc">Rent Low to high</button>
-        <button onClick={()=>rentsortdec()}className="sortByRentDesc">Rent High to low</button>
-        <button onClick={()=>sortareainc()}className="sortByAreaAsc">Area Low to high</button>
-        <button onClick={()=>sortareadec()}className="sortByAreaDesc">Area High to Low</button>
+        <button className="sortById" onClick={()=>{handleId()}}>Sort by ID</button>
+        <button className="sortByRentAsc"  onClick={()=>{handleRent("low")}}>Rent Low to high</button>
+        <button className="sortByRentDesc"  onClick={()=>{handleRent('high')}}>Rent High to low</button>
+        <button className="sortByAreaAsc"  onClick={()=>{handleArea("low")}}>Area Low to high</button>
+        <button className="sortByAreaDesc"  onClick={()=>{handleArea("high")}}>Area High to Low</button>
       </div>
       <input
         className="searchAddress"
         type="text"
         placeholder="Search Address"
+        onChange={(e)=>{handleAreaCode(e.target.value)}}
       />
       <table className="table" border="1">
         <thead>
@@ -29,7 +103,8 @@ export const Rentals = ({result,sortbyid,rentsortinc,rentsortdec,sortareainc,sor
           </tr>
         </thead>
         <tbody>
-          {result.map((house, index) => {
+          {data.map((house) => {
+            
             return (
               <tr key={house.id} className="houseDetails">
                 <td className="houseId">{house.id}</td>
@@ -39,8 +114,9 @@ export const Rentals = ({result,sortbyid,rentsortinc,rentsortdec,sortareainc,sor
                 <td className="areaCode">{house.areaCode}</td>
                 <td className="rent">{house.rent}</td>
                 <td className="preferredTenants">
-                  {/* Show text Both or Bachelors or Married based on values */}
-                  {house.bachelor}
+                  <span>{house.married ? "Married" : "Non-Married"}</span>
+                  <span> / </span>
+                  <span>{house.bachelor ? "Bachelor" : "Non-Bachelor"}</span>
                 </td>
                 <td className="houseImage">
                   <img src={house.image} alt="house" />
